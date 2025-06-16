4175133d85b6625a32364f8298a6c1bab400328f
Vladislav Smolskii <lexus_003@mail.ru>
2025-06-16 07:10:49 +0300

    added login link presentation check

--- test_main_page.py
+++ test_main_page.py
@@ -5,4 +5,4 @@ link = 'http://selenium1py.pythonanywhere.com/'
 def test_guest_can_go_to_login_page(browser):
     page = MainPage(browser, link)
     page.open()
-    page.go_to_login_page()
+    page.should_be_login_link()
