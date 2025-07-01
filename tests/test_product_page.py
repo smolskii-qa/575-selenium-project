52b162011861dfcd9252f565dca5d9b56637f06c
Vladislav Smolskii <lexus_003@mail.ru>
2025-07-01 07:02:52 +0300

    add negative tests in product_page

--- tests/test_product_page.py
+++ tests/test_product_page.py
@@ -3,9 +3,9 @@ import pytest
 
 from pages.product_page import ProductPage
 
-
 # link = 'https://selenium1py.pythonanywhere.com/en-gb/catalogue/the-shellcoders-handbook_209/?promo=newYear'
 # link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019'
+link_without_quiz = ['http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/']
 
 
 @pytest.mark.parametrize(
@@ -33,3 +33,23 @@ def test_guest_can_add_product_to_basket(browser, link):
     page.should_be_correct_product_name_in_success_message()
     page.should_be_correct_amount_in_basket_amount_message()
     time.sleep(3)
+
+@pytest.mark.xfail
+def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
+    page = ProductPage(browser, link_without_quiz)
+    page.open()
+    page.add_to_basket()
+    page.should_not_be_success_message()
+
+
+def test_guest_cant_see_success_message(browser):
+    page = ProductPage(browser, link_without_quiz)
+    page.open()
+    page.should_not_be_success_message()
+
+@pytest.mark.xfail
+def test_message_disappeared_after_adding_product_to_basket(browser):
+    page = ProductPage(browser, link_without_quiz)
+    page.open()
+    page.add_to_basket()
+    page.should_be_success_message_disappeared()
